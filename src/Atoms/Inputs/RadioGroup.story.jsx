5038c796c200328e6840c1cdf120ce33288cfa9c
Jordan Neville <jordann@nulogy.com>
2017-02-20 12:23:04 -0500

    remove unused imports

--- src/Atoms/Inputs/RadioGroup.story.jsx
+++ src/Atoms/Inputs/RadioGroup.story.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { storiesOf, action, linkTo } from '@kadira/storybook';
+import { storiesOf, action } from '@kadira/storybook';
 import cloneDeep from 'lodash/cloneDeep';
 import RadioGroup from './RadioGroup';
 
